6f0383989f8d7c4e4a1378d88e3a2a8479dec74d
ansh98-crypto <61378952+ansh98-crypto@users.noreply.github.com>
2021-03-13 22:47:23 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ form.addEventListener('submit', (e)=>{
     message1.textContent = 'Loading';
     message2.textContent = '';
 
-    fetch(`http://localhost:3000/address?address=${search.value}`).then((response)=>{
+    fetch(`/address?address=${search.value}`).then((response)=>{
     response.json().then(data=>{
         if(data.error){
             message1.textContent = data.error;
--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const utils = require('./util_functions.js')
 const chalk = require('chalk');
 
 const app = express();
+const port = process.env.PORT||3000;
+
 const static_files_path = path.join(__dirname,'../public/');
 const viewsPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
@@ -80,4 +82,4 @@ app.get('*', (req, res)=>{
 })
 
 
-app.listen(3000, () => console.log('Server up and running.'))
+app.listen(port, () => console.log('Server up and running on '+port+'.'))
